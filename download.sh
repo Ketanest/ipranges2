df2660c416cd24734675e02a20d4b17f7c8c85ff
Ketanest <87452756+Ketanest@users.noreply.github.com>
2026-08-31 23:07:05 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 set -x
 
-#curl -L -o asndb.gz "https://ipinfo.io/data/ipinfo_lite.json.gz?_src=frontend&token=xxxxxx"
+curl -L -o asndb.gz "https://ipinfo.io/data/ipinfo_lite.json.gz?_src=frontend&token=xxxxxx"
 
-#gunzip -d asndb.gz
+gunzip -d asndb.gz
 
-#jq -c '{network, asn, as_name}' asndb > asndb-filtered
+jq -c '{network, asn, as_name}' asndb > asndb-filtered
 
 function make_all_ranges(){
 	./export-networks.py -s asndb-filtered -d "$1" -f "$2" --force
