b8ec3e28e9ce2bd791f14b6e1c13ab3e328ce2f8
Ketanest <87452756+Ketanest@users.noreply.github.com>
2026-09-12 10:18:17 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -12,25 +12,24 @@ function make_all_ranges(){
 	./export-networks.py -s asndb-filtered -d "$1" -f "$2" --force
 }
 
-asns=(
-	"google|google"
-	"netflix|netflix"
-	"amazon|amazon"
-	"cloudflare|cloudflare"
-	"facebook|facebook"
-	"apple|apple inc"
-	"github|github"
-	"linode|linode"
-	"microsoft|microsoft"
-	"openai|openai"
-	"oracle|oracle"
-	"telegram|telegram"
-	"vultr|vultr"
-	"twitter|twitter"
-	"proton|proton"
+declare -A asns=(
+	[google]="google"
+	[netflix]="netflix"
+	[amazon]="amazon"
+	[cloudflare]="cloudflare"
+	[facebook]="facebook"
+	[apple]="apple inc"
+	[github]="github"
+	[linode]="linode"
+	[microsoft]="microsoft"
+	[openai]="openai"
+	[oracle]="oracle"
+	[telegram]="telegram"
+	[vultr]="vultr"
+	[twitter]="twitter"
+	[proton]="proton"
 )
 
-for range in "${asns[@]}"; do
-	IFS='|' read -r dir name <<< "$range"
-	make_all_ranges "./$dir" "$name"
+for asn in "${!asns[@]}"; do
+    make_all_ranges "./$asn" "${asns[$asn]}"
 done
